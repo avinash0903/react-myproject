a9d39cac09ac387a7453365712693c731e813eca
avinash0903 <avinash.skzr@gmail.com>
2021-08-14 18:24:48 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-// import React, { Component } from 'react';
-// import Router from '/route';
-
-// function App() {
-//   return (
-//     <>
-//       <Router />
-//     </>
-//   );
-// }
-
-// // export default App;
-
 import React, { Component } from 'react';
-import Route from './route';
+import Router from './route';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Route />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <>
+      <Router />
+    </>
+  );
 }
 
 export default App;
